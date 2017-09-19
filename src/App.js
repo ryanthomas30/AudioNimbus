e64ec5b13b493761d2aa2821c169e81bc2889607
Ryan Thomas <ryanthomas30@gmail.com>
2017-09-18 21:35:30 -0400

    #8 Rename App to Main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import App from 'grommet/components/App';
 import Title from 'grommet/components/Title';
 import HeaderBar from './components/HeaderBar';
 import Article from 'grommet/components/Article';
 import Profile from './components/Profile'
 
-export default () => (
-  <App>
-		<Article>
-			<HeaderBar/>
-			<Profile />
-		</Article>
-  </App>
-);
+class Main extends Component {
+	render() {
+		return (
+			<App>
+				<Article>
+					<HeaderBar/>
+					<Profile />
+				</Article>
+	  	</App>
+		);
+	}
+}
+
+export default Main;
