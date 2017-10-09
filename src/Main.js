0be4aa0ffbfd4d5dca816393d237ae820fd35b23
Ryan Thomas <ryanthomas30@gmail.com>
2017-10-08 20:58:39 -0400

    #16 Landing Page edits

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import App from 'grommet/components/App';
 import HeaderBar from './components/HeaderBar';
+import Box from 'grommet/components/Box';
 import Article from 'grommet/components/Article';
-import AudioPlayer from './components/AudioPlayer';
 import Profile from './components/Profile';
 import LandingPage from './components/LandingPage';
 import { Route } from 'react-router-dom';
@@ -15,8 +15,10 @@ class Main extends Component {
 			<App>
 				<Article>
 					<HeaderBar/>
-					<Route exact path="/profile" component={Profile} />
-					<Route exact path="/" component={LandingPage} />
+					<Box>
+						<Route exact path="/profile" component={Profile} />
+						<Route exact path="/" component={LandingPage} />
+					</Box>
 				</Article>
 			</App>
 		);
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,6 +5,7 @@ import Anchor from 'grommet/components/Anchor';
 import Box from 'grommet/components/Box';
 import Menu from 'grommet/components/Menu';
 import Title from 'grommet/components/Title';
+import Label from 'grommet/components/Label';
 import Section from 'grommet/components/Section';
 import Headline from 'grommet/components/Headline';
 import Paragraph from 'grommet/components/Paragraph';
@@ -16,72 +17,71 @@ class LandingPage extends Component {
   render() {
     return(
       <Box>
-
-      <Article scrollStep={true}
-        controls={true}>
-        <Section pad='large'
-          justify='center'
-          align='center'
-          full='vertical'>
-          <Headline margin='none'>
-            What is AudioNimbus ?
-          </Headline>
-          <Paragraph size='medium'>
-            AudioNimbus is an audio distribution web application for uploading music, podcasts, and other audio, enabling users to share their original audio content. .
-          </Paragraph>
-        </Section>
-        <Section pad='large'
-          justify='center'
-          align='center'
-          full='vertical'
-          colorIndex='accent-1-a'>
-          <Headline margin='none'>
-            You should be able to:
-          </Headline>
-          <Paragraph size='medium'>
-          1. Create a profile page containing audio content.
-          </Paragraph>
-          <Paragraph size='medium'>
-          2. Personalize your profile page.
-          </Paragraph>
-          <Paragraph size='medium'>
-          3. Upload audio content to your profile.
-          </Paragraph>
-          <Paragraph size='medium'>
-          4. View audio content on other user profiles.
-          </Paragraph>
-          <Paragraph size='medium'>
-          5. Listen to audio content on a custom player.
-          </Paragraph>
-        </Section>
-        <Section pad='large'
-          justify='center'
-          align='center'
-          full='vertical'>
-          <Headline margin='none'>
-            Section 3
-          </Headline>
-        </Section>
-        <Section pad='large'
-          justify='center'
-          align='center'
-          full='vertical'
-          colorIndex='accent-1-a'>
-          <Headline margin='none'>
-            Section 4
-          </Headline>
-        </Section>
-        <Section pad='large'
-          justify='center'
-          align='center'
-          full='vertical'>
-          <Headline margin='none'>
-            Section 5
-          </Headline>
-        </Section>
-      </Article>
+	      <Article scrollStep={false}
+	        controls={false}>
+	        <Section pad='large'
+	          justify='center'
+	          align='center'
+	          full='vertical'>
+	          <Headline margin='none'>
+	            What is AudioNimbus?
+	          </Headline>
+	          <Paragraph size='medium' align='center' >
+	            AudioNimbus is an audio distribution web application for uploading
+					music, podcasts, and other audio, enabling users to share their original audio content.
+	          </Paragraph>
+	        </Section>
+	        <Section pad='large'
+	          justify='center'
+	          align='center'
+	          full='vertical'
+	          colorIndex='accent-1-a'>
+		          <Headline margin={{ bottom: 'large' }} >
+		            You should be able to
+		          </Headline>
+		          <Label size='medium' margin='small' >
+		          	Create a profile page containing audio content.
+		          </Label>
+		          <Label size='medium' margin='small' >
+		          	Personalize your profile page.
+		          </Label>
+		          <Label size='medium' margin='small' >
+		          	Upload audio content to your profile.
+		          </Label>
+		          <Label size='medium' margin='small' >
+		          	View audio content on other user profiles.
+		          </Label>
+		          <Label size='medium' margin='small' >
+		          	Listen to audio content on a custom player.
+		          </Label>
+	        </Section>
+	        <Section pad='large'
+	          justify='center'
+	          align='center'
+	          full='vertical'>
+	          <Headline margin='none'>
+	            Section 3
+	          </Headline>
+	        </Section>
+	        <Section pad='large'
+	          justify='center'
+	          align='center'
+	          full='vertical'
+	          colorIndex='accent-1-a'>
+	          <Headline margin='none'>
+	            Section 4
+	          </Headline>
+	        </Section>
+	        <Section pad='large'
+	          justify='center'
+	          align='center'
+	          full='vertical'>
+	          <Headline margin='none'>
+	            Section 5
+	          </Headline>
+	        </Section>
+	      </Article>
       </Box>
-
     );
   }
 }
